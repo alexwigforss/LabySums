bdfe3eacdbdc47cc4e7591f53f6f1bf897f6d6d7
Alex Wigforss <alex.wigforss@gmail.com>
2023-10-19 19:55:36 +0200

    goal remove on == strength

--- GDLabs/LabyRun/colworld.gd
+++ GDLabs/LabyRun/colworld.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
 func _on_goal_body_entered(body):
-	if body.get_name() == "player":
+	var emitting_scene = get_node("/root/colworld/player")
+	var plpower = emitting_scene.strength
+
+	if body.get_name() == "player" && plpower == 20:
 		$youwin.show()
