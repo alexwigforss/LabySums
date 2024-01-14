c7b86956ebaaab5dfcea36254adab755575f39ca
Alex Wigforss <alex.wigforss@gmail.com>
2024-01-14 12:42:59 +0100

    signals tinkering inprog

--- GDLabs/LabyRun/PickNr.gd
+++ GDLabs/LabyRun/PickNr.gd
@@ -12,8 +12,8 @@ func _on_Area2d_body_entered(body):
 	if body.is_in_group("player"):
 		queue_free()# Replace with function body.
 
-func _on_Area2d_area_entered():
-	pass # Replace with function body.
+#func _on_Area2d_area_entered():
+#	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- GDLabs/LabyRun/scenes/Pickable.gd
+++ GDLabs/LabyRun/scenes/Pickable.gd
@@ -6,7 +6,3 @@ func _ready():
 func _on_Area2d_body_entered(body):
 	if body.is_in_group("player"):
 		queue_free()# Replace with function body.
-
-
-func _on_Area2d_area_entered():
-	pass # Replace with function body.
