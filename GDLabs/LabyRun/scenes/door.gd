eaa08f484807ca7e36864dd776e7100db470b46d
Alex Wigforss <alex.wigforss@gmail.com>
2025-06-11 15:17:42 +0200

    Clean N Tweak

--- GDLabs/LabyRun/scenes/door.gd
+++ GDLabs/LabyRun/scenes/door.gd
@@ -8,16 +8,13 @@ signal doormatch(nr)
 func _ready():
 	$Label.text = str(strength)
 
-# TODO Kolla upp var dem !"#¤%a parametrarna skickas ifrån
 func _on_door_body_entered(body, _extra_arg_0): 
-#func _on_door_body_entered(body): 
-	#var emitting_world = get_node("/root/colworld")
 	var emitting_actor = get_node("/root/colworld/player")
 	var plpower = emitting_actor.strength
 
 	if body.get_name() == "player" && plpower == strength:
 		print(plpower," ",self,"removed",_extra_arg_0)
 		emit_signal("doormatch", strength)
-		queue_free()# Replace with function body.
+		queue_free()
 	elif body.get_name() == "player" && plpower != strength:
 		emit_signal("nomatch", strength)	
