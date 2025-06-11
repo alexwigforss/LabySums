bec78321aebea6b042d30c74ab5e358f48435d9b
Alex Wigforss <alex.wigforss@gmail.com>
2025-06-11 16:23:19 +0200

    Cleaned and corrected

--- GDLabs/LabyRun/scenes/door.gd
+++ GDLabs/LabyRun/scenes/door.gd
@@ -8,6 +8,16 @@ signal doormatch(nr)
 func _ready():
 	$Label.text = str(strength)
 
+func get_next_map():
+	var current_map = get_parent()
+	var root = current_map.get_parent()
+	var children = root.get_children()
+	var index = children.find(current_map)
+	
+	if index >= 0 and index < children.size() - 1:
+		return children[index + 1]  # Nästa Map
+	return null  # Finns ingen nästa
+
 func _on_door_body_entered(body, _extra_arg_0): 
 	var emitting_actor = get_node("/root/colworld/player")
 	var plpower = emitting_actor.strength
@@ -17,16 +27,12 @@ func _on_door_body_entered(body, _extra_arg_0):
 		var tilemap_parent = get_parent()
 		if tilemap_parent:
 			var color = tilemap_parent.modulate
-			color.a = 0.5  # t.ex. 30% opacitet
+			color.a = 0.5
 			tilemap_parent.modulate = color
 
-
-		var tilemap_node = get_parent().get_parent().get_node("Map2")  # byt "TileMap" till rätt namn
-		
-		if tilemap_node:
-			var color = tilemap_node.modulate
-			color.a = 1.0  # t.ex. 30% opacitet
-			tilemap_node.modulate = color
+		var next_map = get_next_map()
+		if next_map:
+			next_map.modulate = Color(1, 1, 1, 1.0)  # Ändra transparens t.ex.
 			
 		print(plpower," ",self,"removed",_extra_arg_0)
 		emit_signal("doormatch", strength)
