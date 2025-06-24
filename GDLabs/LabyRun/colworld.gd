1fb06a64bdc50120522e65f58bb84fe79775b376
Alex Wigforss <alex.wigforss@gmail.com>
2025-06-24 14:45:30 +0200

    Issue: #12 Commented out mazegenlab debug prints

--- GDLabs/LabyRun/colworld.gd
+++ GDLabs/LabyRun/colworld.gd
@@ -11,7 +11,6 @@ func _ready():
 func _on_goal_body_entered(body):
 	var emitting_scene = get_node("/root/colworld/player")
 	var plpower = emitting_scene.strength
-
 	if body.get_name() == "player" && plpower == 10:
 		_label.text = "An door has opened"
 	elif body.get_name() == "player" && plpower == 10:
