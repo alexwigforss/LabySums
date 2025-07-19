1d1d20fb795c7ae87b7ebed6f328d0f2505fc783
Alex Wigforss <alex.wigforss@gmail.com>
2025-07-19 16:31:56 +0200

    Issue #31: Animation tree for blending

--- GDLabs/LabyRun/scenes/boss_behave.gd
+++ GDLabs/LabyRun/scenes/boss_behave.gd
@@ -1,15 +1,18 @@
 extends Area2D
 
 onready var anim = $AnimationPlayer
+onready var anim_tree = $AnimationTree
 
 func _ready():
-	while true:
-		anim.play("Strife")
-		yield(anim, "animation_finished")
+	anim.get_animation("ZoomAnim").loop = true
+	anim.get_animation("Strife").loop = true
+	anim.get_animation("Idle").loop = true
+	anim.get_animation("Wiggle").loop = true
 
-#	anim.play("ZoomAnim")
-#	yield(anim, "animation_finished")
-#	anim.play("Strife")
-#	yield(anim, "animation_finished")
-#	anim.play("Wiggle")
-#	yield(anim, "animation_finished")
+	anim_tree.active = true
+	anim_tree.set("parameters/BlendStrife/blend_amount", 0.0)
+	anim_tree.set("parameters/BlendWiggle/blend_amount", 0.0)
+	anim_tree.set("parameters/BlendZoom/blend_amount", 0.0)
+
+func _process(delta):
+	anim_tree.advance(delta)
