291c896ee2d430a70a6bfbfdff988fb90e9d9ce2
Alex Wigforss <alex.wigforss@gmail.com>
2025-07-20 16:59:44 +0200

    Issue #31: Animation reset to start on player enter

--- GDLabs/LabyRun/scenes/boss_behave.gd
+++ GDLabs/LabyRun/scenes/boss_behave.gd
@@ -15,8 +15,18 @@ func _ready():
 	anim_tree.set("parameters/BlendZoom/blend_amount", 0.0)
 
 func begin_strife():
-	anim_tree.set("parameters/BlendStrife/blend_amount", 1.0)
-	
+	if anim_tree != null:
+		anim_tree.set("parameters/BlendStrife/blend_amount", 0.0)
+		anim_tree.set("parameters/BlendWiggle/blend_amount", 1.0)
+		anim_tree.set("parameters/BlendZoom/blend_amount", 0.0)
 
 func _process(delta):
 	anim_tree.advance(delta)
+
+
+func _on_boss_body_entered(body):
+	if not body.is_in_group("player"):
+		return
+	anim_tree.set("parameters/SeekStrife/seek_position", 0.0)
+	anim_tree.set("parameters/SeekWiggle/seek_position", 0.0)
+	anim_tree.set("parameters/SeekZoom/seek_position", 0.0)
