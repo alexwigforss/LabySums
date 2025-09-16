231936b893cd5dac8fe55a1e6388e03b9a7b57aa
Alex Wigforss <alex.wigforss@gmail.com>
2025-09-16 16:24:50 +0200

    Issue #50: Resolved export variables bug

--- GDLabs/LabyRun/actors/enemy_2.gd
+++ GDLabs/LabyRun/actors/enemy_2.gd
@@ -1,16 +1,11 @@
-# TODOS
-# [ ] Start Random
-# [ ] Assemble Strategy
-# [ ] Reward / Punish
-
 extends KinematicBody2D
+
+# The actual start position for reseting
 var start_position: Vector2
-
-# Editable speed
-export var speed := 40.0
+export var speed = 20.0
+# The startposition quantified to the grid
+export var start_pos_ind = Vector2(1,1)
 export var debug_print = false
-
-
 
 var velocity := Vector2.ZERO
 onready var segment = get_node("../..")
@@ -20,23 +15,31 @@ var pause_count := 0.0
 var pause_time := 1.0
 
 var index = 0
-var start_pos := Vector2(3,3)
+var position_index
 var directions = ['down','right','up','left']
-var steps = [2,2,2,2]
+
+var direction_labels = ['down','right','up','left']
+var direction_nums = [1, 1, -1, -1]
+var steps = [6,6,6,6]
 var steps_index = steps [index]
+
+var first_frame = true
 # --- SIGNALS ---
 signal player_hit
 
 
 func _ready():
-	_init()
+	my_init()
 	add_to_group("enemies")
 	#print(segment.numerical_map)
 
-func _init():
-	velocity = Vector2.ZERO
-	position = Vector2(start_pos.y * 16, start_pos.x * 16)
+# _init() is used implicit in _ready()
+func my_init():
 	start_position = position
+	velocity = Vector2.ZERO
+	position = Vector2(start_pos_ind.y * 16, start_pos_ind.x * 16)
+	position_index = Vector2(start_pos_ind.y, start_pos_ind.x)
+	# start_position = position
 	change_direction(directions[index])
 	velocity = velocity.normalized() * speed
 	direction = directions[index]
@@ -60,32 +63,44 @@ func change_direction(new_direction: String) -> void:
 	velocity = new_velocity.normalized() * speed
 	pause_count = pause_time
 
+func look_ahead(dir,pos):
+	pass
+
+func look_at_tile(y,x):
+	return segment.numerical_map[y][x]
 
 func _physics_process(delta):
 	if pause_count > 0.0:
 		pause_count -= delta
 		return
 	
+	velocity = move_and_slide(velocity)
 	match direction:
 		"down":
-			if position.y >= (start_pos.y + steps_index) * 16:
+			if position.y >= (start_pos_ind.y + steps_index) * 16:
 				get_next_direction()
 				change_direction(direction)
 		"right":
-			if position.x >= (start_pos.x + steps_index) * 16:
-				get_next_direction()
-				change_direction(direction)
-		"left":
-			if position.x <= (start_pos.x - steps_index) * 16:
+			if position.x >= (start_pos_ind.x + steps_index) * 16:
 				get_next_direction()
 				change_direction(direction)
 		"up":
-			if position.y <= (start_pos.y - steps_index) * 16:
+			if position.y <= (start_pos_ind.y - steps_index) * 16:
 				get_next_direction()
 				change_direction(direction)
-	# print("Y : ", round(position.y / 16),"  X: ", round(position.x / 16))
-	
-	velocity = move_and_slide(velocity)
+		"left":
+			if position.x <= (start_pos_ind.x - steps_index) * 16:
+				get_next_direction()
+				change_direction(direction)
+	# print("Y : ", round(position.y / 16),"  X: ", round(position.x / 16))	
+	if first_frame:
+		print("At tile 0,0 number is: ", look_at_tile(0,0))
+		print("At tile 1,1 number is: ", look_at_tile(1,1))
+		print("At tile 2,2 number is: ", look_at_tile(2,2))
+		print("At tile 3,3 number is: ", look_at_tile(3,3))
+		print("StartPos = ", start_pos_ind)
+		
+		first_frame = false
 
 
 func get_next_direction():
@@ -95,14 +110,15 @@ func get_next_direction():
 	direction = directions[index]
 	steps_index = steps[index]
 	
-	start_pos.y = round(position.y / 16)
-	start_pos.x = round(position.x / 16)
+	start_pos_ind.y = round(position.y / 16)
+	start_pos_ind.x = round(position.x / 16)
 	
 	if debug_print:
+		print("New Startpos set: ", start_pos_ind)
 		print("Got direction ", index, " ", direction)
 
 func reset_to_start() -> void:
-	_init()
+	my_init()
 
 func _on_Area2D_body_entered(body) -> void:
 	if body.is_in_group("player"):
